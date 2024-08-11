b6ba39f25f171e09f1c7d00245257434181b40a3
aniket <aniket@microsec.ai>
2024-08-11 13:32:32 -0400

    fix: django rquirements and dockerfile for django postgres setup, update: docker-compose scripts

--- Dockerfile
+++ Dockerfile
@@ -1,25 +1,33 @@
 # python image
 FROM python:3.10-slim
 
-# set the working directory
+# Set the working directory
 WORKDIR /app
 
-# copy and install requirements
+# Copy and install requirements
 COPY requirements.txt ./
+
+# Install system dependencies
+RUN apt-get update && \
+    apt-get install -y \
+    libpq-dev \ 
+    python3-dev \
+    build-essential \
+    --no-install-recommends && \
+    apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+# Install Python dependencies
 RUN pip install -r requirements.txt
 
-# copy rest of the application code
-COPY  . .
+# Copy rest of the application code
+COPY . .
 
-# collect static files
+# Collect static files (if needed)
 # RUN python manage.py collectstatic --noinput
 
-# Expose to port 8000
+# Expose port 8000
 EXPOSE 8000
 
-# start django server
-
+# Start Django server
 CMD ["uvicorn", "test_server_django.asgi:application", "--host", "0.0.0.0", "--port", "8000"]
-
-
-
--- test_server_django/settings.py
+++ test_server_django/settings.py
@@ -26,8 +26,12 @@ SECRET_KEY = "django-insecure-lwhb160*=$!cz+$cs89#t9_ta&guo(d8x0euu44f4trj0botc1
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
+NAME = os.getenv('NAME')
+from loguru import logger
+logger.info(f" this is your environment variable {NAME}")
 # Application definition
 
 INSTALLED_APPS = [
@@ -74,9 +78,13 @@ WSGI_APPLICATION = "test_server_django.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 
